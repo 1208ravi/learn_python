72e7d1c989cf7e765bd2e28ea318163338c50ec9
1208ravi <chinthamravindrareddy@gmail.com>
2017-04-15 18:10:16 +0530

    Update file_operations.py

--- file_operations.py
+++ file_operations.py
@@ -1,7 +1,7 @@
-
-grades = {'Bart':75, 'Lisa':98, 'Milhouse':80, 'Nelson':65}
-
 import pickle              # import module first
+
+# grades variable
+grades = {'Bart':75, 'Lisa':98, 'Milhouse':80, 'Nelson':65}
 
 f = open('gradesdict.p', 'w')   # Pickle file is newly created where foo1.py is
 pickle.dump(grades, f)          # dump data to f
